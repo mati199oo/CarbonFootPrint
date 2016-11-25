7d200481b59aa36f95d245949850b077ef785e50
mfurmane <mfurmane9@gmail.com>
2016-11-25 13:06:37 +0100

    added mutation

--- src/main/java/solver/Solver.java
+++ src/main/java/solver/Solver.java
@@ -6,6 +6,8 @@ import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Random;
 
 /**
  * Created by Ja on 2016-11-07.
@@ -14,6 +16,7 @@ public class Solver {
 
     private HashMap<String, ArrayList<String>> types;
     private HashMap<String, Action> actions;
+//    private HashMap<String, List<Action>> actionsByType; 
     private String target;
 
     private CopyMaker copyMaker = new CopyMaker();
@@ -24,20 +27,48 @@ public class Solver {
         this.actions = actions;
         this.target = target;
     }
+    
+    private Action switchOnType(Action actual) {
+    	List<String> substitutes = new ArrayList<String>();
+    	substitutes.addAll(types.get(actual.getType()));
+    	substitutes.remove(actual.getTitle());
+    	Random rand = new Random();
+    	if (substitutes.isEmpty())
+    		return actual;
+    	else
+    		return actions.get(substitutes.get(rand.nextInt(substitutes.size())));
+    }
 
     public void solve() {
     	System.out.println(target);
         Action defaultSolution = createDefaultSolution(target);
+        Action maxMutatedSolution = createMaxMutatedSolution(target);
         HashMap<String, Double> result = calculateFootprintCost(defaultSolution);
-        System.out.println(result.get("CarbonCost"));
+        System.out.println("default: " + result.get("CarbonCost"));
+        result = calculateFootprintCost(maxMutatedSolution);
+        System.out.println("maxMutated: " + result.get("CarbonCost"));
     }
 
-    private Action createDefaultSolution(String defaultAction) {
+	private Action createDefaultSolution(String defaultAction) {
         Action action = copyMaker.copyAction(actions.get(defaultAction));
         ArrayList<Action> footprintActions = new ArrayList<Action>();
         for (String defaultActionName: action.getDefaultActions()) {
             Action defaultActionObject = createDefaultSolution(defaultActionName);
             footprintActions.add(defaultActionObject);
+        }
+        action.setFootprintActions(footprintActions);
+        return action;
+    }
+	
+	private Action createMaxMutatedSolution(String Action) {
+        Action action = copyMaker.copyAction(actions.get(Action));
+        ArrayList<Action> footprintActions = new ArrayList<Action>();
+        for (String ActionName: action.getDefaultActions()) {
+        	Action a = actions.get(ActionName);
+        	a = switchOnType(a);
+        	if (a != actions.get(ActionName)) System.out.println(ActionName + " -> " + a.getTitle());
+            Action ActionObject = createMaxMutatedSolution(a.getTitle());
+            footprintActions.add(ActionObject);
         }
         action.setFootprintActions(footprintActions);
         return action;
